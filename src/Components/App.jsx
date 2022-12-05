5732e776d4031eb90b8d2d3054ef3a39363e03fd
Glad <lukegladys615@gmail.com>
2022-12-05 15:21:09 +0100

    first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Products from '../Components/products'
+
+function App() {
+  return (
+    <div>
+        <Products />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/products.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image1 from "../Assets/images/image-1.png";
+// import Image2 from "../Assets/images/image-2.png";
+// import Image3 from "../Assets/images/image-3.png";
+// import Image4 from "../Assets/images/image-4.png";
+// import Image5 from "../Assets/images/image-5.png";
+// import Image6 from "../Assets/images/image-6.png";
+// import Image7 from "../Assets/images/image-7.png";
+// import Image8 from "../Assets/images/image-8.png";
+
+
+function Products() {
+  return (
+    <div>
+        <img className='h-20 w-20' src={Image1} alt="furniture" />
+    </div>
+  )
+}
+
+export default Products
