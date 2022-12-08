b2604e8605bcd8ca73d774614338bdf5b6aab9f2
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-12-08 10:51:36 +0100

    completed the Queries

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,10 +14,10 @@ function Header() {
           className=" p-1 bg-nav"
         />
         <section className="w-widthH h-headerH flex flex-col justify-center leading-3 tracking-wide lg:absolute lg:-top-10 lg:ml-20 pl-6 bg-pops lg:pl-16 lg:pr-6">
-          <h1 className="lg:w-80 text-5xl">
+          <h1 className="lg:w-80 lg:text-5xl text-3xl w-72">
             High-Quality Furniture Just For You
           </h1>
-          <p className="mt-4 lg:w-80 mr-8 mb-4 text-lg text-grey font-semibold">
+          <p className="mt-4 w-72 lg:w-80 mr-8 mb-4 text-lg text-grey font-semibold">
             Our furniture is made from selected and best quality materials that
             are suitable for your dream home
           </p>
