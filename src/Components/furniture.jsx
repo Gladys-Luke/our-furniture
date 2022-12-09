31d5b363ce90a63f01f9ad315e03445c206b5db6
Glad <lukegladys615@gmail.com>
2022-12-09 13:12:50 +0100

    final final

--- src/Components/furniture.jsx
+++ src/Components/furniture.jsx
@@ -21,19 +21,19 @@ function Furniture() {
         data-aos-easing="ease-out-cubic"
         data-aos-duration="2000"
       className='flex flex-col gap-2 lg:flex-row xl:flex-ro md:flex-row sm:flex-col '>
-      <img  src='/assets/images/Rectangle-1.png' alt='' />  
-      <img className='h-72 m-auto' src='/assets/images/Rectangle-2.png' alt='' />  
-      <img className='h-80 mt-12' src='/assets/images/Rectangle-3.png' alt='' />  
+      <img  src='/Assets/images/Rectangle-1.png' alt='' />  
+      <img className='h-72 m-auto' src='/Assets/images/Rectangle-2.png' alt='' />  
+      <img className='h-80 mt-12' src='/Assets/images/Rectangle-3.png' alt='' />  
       </div>
       <div 
         data-aos="flip-left"
         data-aos-easing="ease-out-cubic"
         data-aos-duration="2000"
         className='flex flex-col gap-2 lg:flex-row xl:flex-ro md:flex-row sm:flex-col'>
-      <img src='/assets/images/Rectangle-4.png' alt='' />  
-      <img className='w-80 mb-16' src='/assets/images/Rectangle-5.png' alt='' />  
-      <img className='mb-10' src='/assets/images/Rectangle-6.png' alt='' />  
-      <img className='ml-4 ' src='/assets/images/Rectangle-7.png' alt='' />  
+      <img src='/Assets/images/Rectangle-4.png' alt='' />  
+      <img className='w-80 mb-16' src='/Assets/images/Rectangle-5.png' alt='' />  
+      <img className='mb-10' src='/Assets/images/Rectangle-6.png' alt='' />  
+      <img className='ml-4 ' src='/Assets/images/Rectangle-7.png' alt='' />  
       </div>
     </div>
     </div>
