37399f28a327afa1241f5014d23edf72e7543739
Glad <lukegladys615@gmail.com>
2022-12-09 13:03:14 +0100

    last com

--- src/products.jsx
+++ src/products.jsx
@@ -9,134 +9,130 @@ import React from "react";
 // import Image7 from "../Assets/images/image-7.png";
 // import Image8 from "../Assets/images/image-8.png";
 
-
 const Products = [
   {
     name: "Syltherine",
-    img: '/assets/images/image-1.png',
+    img: "/Assets/images/image-1.png",
     type: "Stylish cafe chair",
     price: "Rp 2.500.000",
     old: "Rp 3.500.000",
   },
   {
     name: "Leviosa",
-    img: '/assets/images/image-2.png',
+    img: "/Assets/images/image-2.png",
     type: "Stylish cafe chair",
-    price: "Rp 2.500.000"
+    price: "Rp 2.500.000",
   },
   {
     name: "Lolito",
-    img: '/assets/images/image-3.png',
+    img: "/Assets/images/image-3.png",
     type: "Luxury big sofa",
     price: "Rp 7.000.000",
     old: "Rp 14.000.000",
   },
   {
     name: "Respira",
-    img: '/assets/images/image-4.png',
+    img: "/Assets/images/image-4.png",
     type: "Minimalist fan",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Grifo",
-    img: '/assets/images/image-5.png',
+    img: "/Assets/images/image-5.png",
     type: "Night lamp",
-    price: "Rp 1.500.000"
+    price: "Rp 1.500.000",
   },
   {
     name: "Muggo",
-    img: '/assets/images/image-6.png',
+    img: "/Assets/images/image-6.png",
     type: "Small mug",
-    price: "Rp 150.000"
+    price: "Rp 150.000",
   },
   {
     name: "Pingky",
-    img: '/assets/images/image-7.png',
+    img: "/Assets/images/image-7.png",
     type: "Cute bed set",
     price: "Rp 7.000.000",
     old: "Rp 14.000.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-8.png',
+    img: "/Assets/images/image-8.png",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-9.jpg',
+    img: "/Assets/images/image-9.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-10.jpg',
+    img: "/Assets/images/image-10.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-11.jpg',
+    img: "/Assets/images/image-11.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-12.jpg',
+    img: "/Assets/images/image-12.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-13.jpg',
+    img: "/Assets/images/image-13.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-14.jpg',
+    img: "/Assets/images/image-14.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-15.jpg',
+    img: "/Assets/images/image-15.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-16.jpg',
+    img: "/Assets/images/image-16.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-17.jpg',
+    img: "/Assets/images/image-17.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-18.jpg',
+    img: "/Assets/images/image-18.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-19.jpg',
+    img: "/Assets/images/image-19.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
   {
     name: "Potty",
-    img: '/assets/images/image-20.jpg',
+    img: "/Assets/images/image-20.jpg",
     type: " Minimalist flower pot",
-    price: "Rp 500.000"
+    price: "Rp 500.000",
   },
- 
-  
-   
 ];
 
 export default Products;
